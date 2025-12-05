4dac7d2ea24593304ec1f1645b1e3c594633c621
Minze <gustavogarone@protonmail.com>
2025-12-05 19:28:18 -0300

    finaliza primeira versão do relatorio

--- final/.conform.5696982.relfinal.qmd.4.r
+++
@@ -1,8 +0,0 @@
-#| output: false
-library(dplyr)
-library(tidyr)
-library(stringr)
-library(lme4)
-library(lmerTest)
-library(car)
-library(knitr)
--- final/.conform.9472460.relfinal.qmd.1.r
+++
@@ -1,3 +0,0 @@
-#| label: tbl-dados
-#| tbl-cap: "Dados transformados em tabela longa para análise"
-kable(df_long)
